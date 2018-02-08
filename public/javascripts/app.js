d4eca05c7d06d22c1b6dbc947391d71aa69f1291
liliexuan <liliexuan@163.com>
2018-02-09 00:58:49 +0800

    乱七八糟的

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,14 @@
 // For any third party dependencies, like jQuery, place them in the lib folder.
 
 // Configure loading modules from the lib directory,except for 'app' ones, which are in a sibling directory.
-require.config({
+requirejs.config({
     paths: {
-        app: '../app',
-        jQuery: 'https://cdn.jsdelivr.net/npm/jquery@3.2/dist/jquery.min.js',
-        vue: 'https://cdn.jsdelivr.net/npm/vue@2.5.13/dist/vue.js',
-        router: 'https://unpkg.com/vue-router@3.0.1/dist/vue-router.js',
-        element: 'https://unpkg.com/element-ui@2.1.0/lib/index.js'
+        jQuery: 'https://cdn.jsdelivr.net/npm/jquery@3.2/dist/jquery.min',
+        vue: 'https://cdn.jsdelivr.net/npm/vue@2.5.13/dist/vue',
+        VueRouter: 'https://unpkg.com/vue-router@3.0.1/dist/vue-router',
+        ELEMENT: 'https://unpkg.com/element-ui@2.1.0/lib/index'
     }
 });
 
 // Start loading the main app file. Put all of your application logic in there.
-require(['javascripts/app/main']);
+requirejs(['app/main']);
--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -1,3 +1,14 @@
-define(['require'], function(require) {
-    
+define(['vue', 'VueRouter', 'ELEMENT', 'app/routers'], function (Vue, VueRouter, element, routers) {
+    Vue.use(element);
+    // 3. 创建 router 实例，然后传 `routes` 配置
+    // 你还可以传别的配置参数, 不过先这么简单着吧。
+    const router = new VueRouter({
+        routes // （缩写）相当于 routes: routes
+    })
+    const app = new Vue({
+        router: router,
+        data: {
+            visible: false
+        }
+    }).$mount('#app');
 });
